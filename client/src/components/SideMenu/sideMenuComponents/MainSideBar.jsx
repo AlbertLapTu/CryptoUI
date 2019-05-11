7a5e1b64b55236b61a80bf935e6768032fba8b24
Your Name <you@example.com>
2019-05-11 10:35:10 -0700

    Add services panel

--- client/src/components/SideMenu/sideMenuComponents/MainSideBar.jsx
+++ client/src/components/SideMenu/sideMenuComponents/MainSideBar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SideMenuStyle from '../styles/SideMenuStyles.jsx';
 import Logo from '../sideMenuComponents/Logo.jsx';
 import QuickAccess from './QuickAccess.jsx';
+import Services from './Services.jsx';
 
 const MainSideBar = () => {
   return (
     <SideMenuStyle>
       <Logo />
       <QuickAccess />
+      <Services />
     </SideMenuStyle>
   );
 };
