03daa87417baeb64fa184dd262ce88befa3925f1
Your Name <you@example.com>
2019-05-11 10:23:18 -0700

    Added quickAccess toolbar

--- client/src/components/SideMenu/sideMenuComponents/MainSideBar.jsx
+++ client/src/components/SideMenu/sideMenuComponents/MainSideBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SideMenuStyle from '../styles/SideMenuStyles.jsx';
 import Logo from '../sideMenuComponents/Logo.jsx';
+import QuickAccess from './QuickAccess.jsx';
 
 const MainSideBar = () => {
   return (
     <SideMenuStyle>
       <Logo />
+      <QuickAccess />
     </SideMenuStyle>
   );
 };
