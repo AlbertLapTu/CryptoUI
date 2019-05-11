b3e5dc8138f2afa323ae5dec6f79e6f3910b34f0
Your Name <you@example.com>
2019-05-11 10:24:08 -0700

    Load app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import MainSideBar from './components/SideMenu/sideMenuComponents/MainSideBar.jsx';
 
-const App = () => <h1>Hello World</h1>;
+const App = () => <MainSideBar />;
 
 export default App;
