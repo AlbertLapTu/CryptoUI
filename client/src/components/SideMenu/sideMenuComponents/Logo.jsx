93402554d03d43a24b1cadf304368b2661ef050f
Your Name <you@example.com>
2019-05-11 10:23:00 -0700

    Logo branding bar added. Requires center styles

--- client/src/components/SideMenu/sideMenuComponents/Logo.jsx
+++ client/src/components/SideMenu/sideMenuComponents/Logo.jsx
@@ -5,32 +5,35 @@ import styled from 'styled-components';
 const LogoImage = styled.img`
   width: 32px;
   height: 32px;
-  margin: 19px 163px 973px 22px;
-  position: absolute;
 `;
 
 const LogoName = styled.div`
   font-size: 20px;
+  font-weight: 300;
   color: #fff;
-  width: 97px;
-  height: 19px;
   letter-spacing: 2px;
-  position: relative;
 `;
 
-const Line = styled.div`
+//TODO: Vertical align the container
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const Line = styled.hr`
   width: 175px;
   height: 1px;
   opacity: 0.3;
   border: solid 0.2px #f8f9fb;
-  margin: 60px auto;
 `;
 
 const Logo = () => {
   return (
     <React.Fragment>
-      <LogoImage src={c} />
-      <LogoName>CoinUI</LogoName>
+      <LogoContainer>
+        <LogoImage src={c} />
+        <LogoName>CoinUI</LogoName>
+      </LogoContainer>
       <Line />
     </React.Fragment>
   );
