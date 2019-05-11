e709d2e890dcc6807e5fc39b5f25acf31e761541
Your Name <you@example.com>
2019-05-11 11:51:05 -0700

    Include Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import MainSideBar from './components/SideMenu/sideMenuComponents/MainSideBar.jsx';
+import TopHeader from './components/Header/TopHeader.jsx';
+import styled from 'styled-components';
 
-const App = () => <MainSideBar />;
+//TODO: Wrap entire app with general global styles to have components fall in line
+const GlobalStyle = styled.div`
+  display: inline;
+`;
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <GlobalStyle>
+        <TopHeader />
+        <MainSideBar />
+      </GlobalStyle>
+    </React.Fragment>
+  );
+};
 
 export default App;
