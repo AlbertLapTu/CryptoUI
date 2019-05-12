14821e976d2f1846b2391cd81e1c70d0cb46ee3a
Your Name <you@example.com>
2019-05-11 18:01:21 -0700

    Update TopHeaderStyles

--- client/src/components/Header/styles/TopHeaderStyles.jsx
+++ client/src/components/Header/styles/TopHeaderStyles.jsx
@@ -5,7 +5,8 @@ const TopHeaderStyle = styled.div`
   height: 70px;
   box-shadow: 0 8px 28px 0 #48596619;
   background-color: #ffffff;
-  padding-left: 217px;
+  display: flex;
+  align-items: center;
 `;
 
 export default TopHeaderStyle;
