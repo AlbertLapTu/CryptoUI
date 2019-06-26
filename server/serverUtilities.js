f3a18af6f437344706e38d6bf248791d9a82b832
Your Name <itzalbert2@gmail.com>
2019-06-25 21:31:04 -0700

    Add utility functions for server

--- server/serverUtilities.js
+++ server/serverUtilities.js
@@ -1,10 +1,21 @@
+const fs = require('fs');
+
 module.exports = {
   formatCryptoTimes: request => {
+    let output = request;
     request.Data.forEach(entry => {
       entry.time = new Date(entry.time * 1000).toLocaleDateString();
     });
+
+    return output;
   },
-  writeToDisk: () => {
-    console.log('Still need to do');
+  writeToDisk: apiData => {
+    fs.writeFile('cryptoCompareData.json', apiData, err => {
+      if (err) {
+        throw new Error('Error receiving data');
+      } else {
+        console.log('File Saved!');
+      }
+    });
   }
 };
